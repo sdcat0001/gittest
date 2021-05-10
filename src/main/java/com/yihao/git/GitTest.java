005801f6373171c0e0e5442e716328fb6b181007
hyjx01 <1907275131@qq.com>
2021-05-10 22:18:43 +0800

    first commit

---
+++ src/main/java/com/yihao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yihao.git;
+
+/**
+ * @author hyjx
+ * @create 2021-05-10 22:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
